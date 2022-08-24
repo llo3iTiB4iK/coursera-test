a270c5770fc54bd5aad74a2b66ec9ff6bf3184ab
llo3iTiB4iK <90177277+llo3iTiB4iK@users.noreply.github.com>
2022-08-24 15:57:06 +0300

    Delete module4-solution directory

--- module4-solution/SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-var helloSpeaker = {};
-helloSpeaker.speak=function(name) {
-  speakWord="Hello";
-  console.log(speakWord + " " + name);
-}
-window.helloSpeaker=helloSpeaker;
-})(window);
